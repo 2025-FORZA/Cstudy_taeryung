b5f8f952767f4e9514b47f92ff7ecdf3e68953b1
“taeryung” <goldribbon107@naver.com>
2025-04-02 18:37:07 +0900

    1_Lab02

---
+++ 1_Lab02_1.c
@@ -0,0 +1,9 @@
+// 1_Lab02_1
+//πÈ¡ÿ 11382 ≤ø∏∂¡§πŒ
+#include <stdio.h>
+int main() {
+	long int A, B, C;
+	scanf("%ld %ld %ld", &A, &B, &C);
+	printf("%ld", A + B + C);
+	return 0;
+}
---
+++ 1_Lab02_2.c
@@ -0,0 +1,12 @@
+// 1_Lab02_2
+//백준 10430 나머지
+#include <stdio.h>
+int main() {
+	int A, B, C;
+	scanf("%d %d %d", &A, &B, &C);
+	printf("%d\n", (A + B) % C);
+	printf("%d\n", ((A % C) + (B % C)) % C);
+	printf("%d\n", (A * B) % C);
+	printf("%d", ((A % C) * (B % C)) % C);
+	return 0;
+}
